5480210a41a338ef76f050524a8924cc2e4fef9e
Gleb <glebgorelov0110@mail.ru>
2022-06-10 17:49:22 +0700

    user authorization in the admin panel

--- snippets/views.py
+++ snippets/views.py
@@ -1,8 +1,10 @@
 from snippets.models import  Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
-
-
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 # class SnippetList(mixins.ListModelMixin,  #рефакторинг кода с помощью миксинов
 #                   mixins.CreateModelMixin,
 #                   generics.GenericAPIView):
@@ -19,12 +21,24 @@ from rest_framework import generics
 class SnippetList(generics.ListCreateAPIView):
     queryset=Snippet.objects.all()
     serializer_class=SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     queryset=Snippet.objects.all()
     serializer_class=SnippetSerializer
 
 
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 # class SnippetList(APIView):
